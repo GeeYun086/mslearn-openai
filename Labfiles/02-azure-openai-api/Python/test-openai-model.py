91d89ef6ff8611cd3581ecce67112f6af15c959d
JeeYun098 <gyjung086@gmail.com>
2025-11-13 10:58:47 +0900

    Update test-openai-model.py

--- Labfiles/02-azure-openai-api/Python/test-openai-model.py
+++ Labfiles/02-azure-openai-api/Python/test-openai-model.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Add Azure OpenAI package
-
+from openai import AzureOpenAI
 
 def main(): 
         
@@ -15,9 +15,14 @@ def main():
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
         
         # Initialize the Azure OpenAI client...
+        client = AzureOpenAI(  
+            azure_endpoint=azure_oai_endpoint,  
+            api_key=azure_oai_key,  
+            api_version="2025-01-01-preview",
+        )
+
+        system_message = """당신은 친절한 AI 도우미입니다."""
         
-
-
         while True:
             # Get input text
             input_text = input("Enter the prompt (or type 'quit' to exit): ")
@@ -27,12 +32,24 @@ def main():
                 print("Please enter a prompt.")
                 continue
 
-            print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
+            print("\nSending request to Azure OpenAI endpoint...\n\n")
             
             # Add code to send request...
+            messages_array = [
+                {"role":"system","content":system_message},
+                {"role":"user","content":input_text}
+            ]
             
-            
-            
+            response = client.chat.completions.create(
+                model = azure_oai_deployment,
+                temperature = 0.7,
+                max_tokens = 1200,
+                messages=messages_array
+            )
+            generated_text = response.choices[0].message.content
+            messages_array.append({"role":"assistant","content":generated_text})
+            print("Answer: " + generated_text + "\n")
+
 
     except Exception as ex:
         print(ex)
